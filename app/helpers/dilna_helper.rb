8da065c6113f5a4bb11426b247f85cbcc27b1817
doooby <zelazk.o@email.cz>
2014-08-21 20:38:47 +0200

    posun_blok alfa 1

--- app/helpers/dilna_helper.rb
+++ app/helpers/dilna_helper.rb
@@ -5,16 +5,13 @@ module DilnaHelper
       when String
         content_for :hrad_head, javascript_include_tag(arr)
       when Array
-        arr.each{|file| content_for :hrad_head, javascript_include_tag(file)}
+        content_for :hrad_head, arr.each{|file| javascript_include_tag(file)}.join.html_safe
     end
     content_for :hrad_head, yield if block_given?
   end
 
-  def self_chat_okno(who)
-    content_tag :div, class: 'self_chat', data: {who: who} do
-      inner = content_tag :div, nil, class: 'vypis'
-      inner += text_area_tag "#{who}_zapis", nil, class: 'zapis', size: '30x3'
-      inner += select_tag 'to_whom', options_for_select([['všichni', 'a'],['server', 's']])
-    end
+  def link_na_prostranstvi(napis, projekt)
+    link_to napis, dilna_prostranstvi_path(projekt: projekt)
   end
+
 end
