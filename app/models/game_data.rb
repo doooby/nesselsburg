ec22fbcc0df986c041fe8c932c243bca71ef394c
doooby <zelazk.o@email.cz>
2015-05-10 16:21:41 +0200

    deploy 0.2.0 #2

--- app/models/game_data.rb
+++ app/models/game_data.rb
@@ -1,13 +1,13 @@
-class GameData < ActiveRecord::Base
-
-  validate :validate_uniqueness
-
-  def validate_uniqueness
-    errors.add :base, "Combination game=#{game} & key=#{key} id not unique." if self.class.exists? game: game, key: key
-  end
-
-  scope :game, -> (game) { where game: game }
-  scope :key, -> (key) { where key: key }
-  scope :key_begins, -> (prefix) { where GameData.arel_table[:key].matches("#{prefix}%") }
-
-end
+# class GameData < ActiveRecord::Base
+#
+#   validate :validate_uniqueness
+#
+#   def validate_uniqueness
+#     errors.add :base, "Combination game=#{game} & key=#{key} id not unique." if self.class.exists? game: game, key: key
+#   end
+#
+#   scope :game, -> (game) { where game: game }
+#   scope :key, -> (key) { where key: key }
+#   scope :key_begins, -> (prefix) { where GameData.arel_table[:key].matches("#{prefix}%") }
+#
+# end
--- db/migrate/20141001204945_create_game_data.rb
+++ db/migrate/20141001204945_create_game_data.rb
@@ -1,10 +1,10 @@
-class CreateGameData < ActiveRecord::Migration
-  def change
-    create_table :game_data do |t|
-      t.text :game
-      t.text :key
-      t.text :data
-      t.timestamps
-    end
-  end
-end
+# class CreateGameData < ActiveRecord::Migration
+#   def change
+#     create_table :game_data do |t|
+#       t.text :game
+#       t.text :key
+#       t.text :data
+#       t.timestamps
+#     end
+#   end
+# end
